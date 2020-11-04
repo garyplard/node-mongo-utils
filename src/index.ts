f7e6970370db20f6e1daba847215857c79ea75c9
Gary Plard <gary.plard@didask.com>
2020-11-04 22:26:37 +0100

    Fixes binaries paths

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import { getInstalledPathSync } from 'get-installed-path'
+import { name } from '../package.json'
 
 export * from './mongorestore'
 export * from './mongodump'
 export * from './mongoclone'
 export * from './utils'
 
-const binFolderPath = `${getInstalledPathSync(require('../package.json').name, { local: true })}/bin`
+const rootFolder = getInstalledPathSync(name, { paths: process.mainModule?.paths })
 const binariesList = ['mongodump', 'mongorestore'] as const
 
 export const binaries = binariesList.reduce(
   (acc, filename) => { 
-    acc[filename] = `${binFolderPath}/${filename}`
+    acc[filename] = `${rootFolder}/bin/${filename}`
     return acc
   }, 
   {} as Record<typeof binariesList[number], string>
